474ea8bc00537d1ce8a0ec85ffeb8782d773d5c1
Michael C. Grant <mcg@cvxr.com>
2018-06-29 18:44:59 -0500

    Try fully inlined appveyor

--- ci/appveyor/script.cmd
+++
@@ -1,9 +0,0 @@
-@echo on
-
-REM Anaconda
-cd %APPVEYOR_BUILD_FOLDER%\conda_recipe
-
-call conda build --python %PYTHON_VERSION% osqp --output-folder conda-bld\
-if errorlevel 1 exit /b 1
-
-@echo off
