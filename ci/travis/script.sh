ab55531854d37c290bfbb483d6f509a0dad60b98
Michael C. Grant <mcg@cvxr.com>
2018-09-02 12:53:39 -0500

    Travis inlining

--- ci/travis/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-# Anaconda
-source ${DEPS_DIR}/miniconda/bin/activate
-
-echo "Creating conda package..."
-cd ${TRAVIS_BUILD_DIR}/conda_recipe
-conda build --python ${PYTHON_VERSION} osqp --output-folder conda-bld/
